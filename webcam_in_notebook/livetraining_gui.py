d92243e4221ea53888c6ab005b65a082f01380f7
Sigmund Rolfsjord <sigmund-johannes-ljosvoll.rolfsjord@ffi.no>
2020-04-22 15:23:04 +0200

    test

--- webcam_in_notebook/livetraining_gui.py
+++ webcam_in_notebook/livetraining_gui.py
@@ -4,7 +4,7 @@ from google.colab.patches import cv2_imshow
 from IPython.display import display
 from IPython.display import HTML
 from IPython.display import clear_output
-from numpy import np
+import numpy as np
 import cv2
 import tensorflow as tf
 
