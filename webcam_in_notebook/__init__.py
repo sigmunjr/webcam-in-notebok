47ae89ae668d04608eec31fb0011a99e8b7982ab
Sigmund Rolfsjord <sigmund-johannes-ljosvoll.rolfsjord@ffi.no>
2020-04-22 14:25:57 +0200

    test #3

--- webcam_in_notebook/__init__.py
+++ webcam_in_notebook/__init__.py
@@ -1,3 +1,3 @@
-from webcam_in_notebook.webcam_in_notebook import *
-from webcam_in_notebook.livetraining_gui import *
+from .webcam_in_notebook import *
+from .livetraining_gui import *
 print('imported stuff')
